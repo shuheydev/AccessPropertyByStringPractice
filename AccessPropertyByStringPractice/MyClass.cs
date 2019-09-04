23283488d6305193dc4dee829869e7687cae46ff
shuhey <nishizawa@timedia.co.jp>
2019-09-04 22:44:19 +0900

    Add project files.

---
+++ AccessPropertyByStringPractice/MyClass.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+namespace AccessPropertyByStringPractice
+{
+    public class MyClass
+    {
+        public int MyProperty1 { get; }
+        public int MyProperty2 { get; }
+        public int MyProperty3 { get; }
+
+        public MyClass()
+        {
+            MyProperty1 = 10;
+            MyProperty2 = 20;
+            MyProperty3 = 30;
+        }
+
+        public int this[string propertyName]
+        {
+            get
+            {
+                return (int)typeof(MyClass).GetProperty(propertyName).GetValue(this);
+            }
+            set
+            {
+                typeof(MyClass).GetProperty(propertyName).SetValue(this, (int)value);
+            }
+        }
+    }
+}
---
+++ AccessPropertyByStringPractice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AccessPropertyByStringPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClass = new MyClass();
+
+            int val = myClass["MyProperty3"];
+
+            Console.WriteLine($"MyProperty3：{val + 100}");//出力 -> MyProperty3：30
+        }
+    }
+}
